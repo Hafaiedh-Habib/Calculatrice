06afa934738f7f969b1d56d16f8a9801867d987c
hafai <hafai@PC_HAMED>
2024-12-04 21:29:57 +0100

    Ajoutez des fichiers projet.

---
+++ Calculatrice/Form1.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btn1 = New Button()
+        btn2 = New Button()
+        btn3 = New Button()
+        btn4 = New Button()
+        btn5 = New Button()
+        btn6 = New Button()
+        btn7 = New Button()
+        btn8 = New Button()
+        btn9 = New Button()
+        btnPLUS = New Button()
+        btnMOINS = New Button()
+        btnFOIS = New Button()
+        btnAC = New Button()
+        btnRT = New Button()
+        btnSUR = New Button()
+        btn0 = New Button()
+        btnCLOSE = New Button()
+        txt = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btn1
+        ' 
+        btn1.Location = New Point(25, 372)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(70, 70)
+        btn1.TabIndex = 0
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Location = New Point(113, 372)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(70, 70)
+        btn2.TabIndex = 1
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btn3
+        ' 
+        btn3.Location = New Point(195, 372)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(70, 70)
+        btn3.TabIndex = 2
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Location = New Point(25, 296)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(70, 70)
+        btn4.TabIndex = 3
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Location = New Point(113, 296)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(70, 70)
+        btn5.TabIndex = 4
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btn6
+        ' 
+        btn6.Location = New Point(195, 296)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(70, 70)
+        btn6.TabIndex = 5
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Location = New Point(25, 220)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(70, 70)
+        btn7.TabIndex = 6
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' btn8
+        ' 
+        btn8.Location = New Point(113, 220)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(70, 70)
+        btn8.TabIndex = 7
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Location = New Point(195, 220)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(70, 70)
+        btn9.TabIndex = 8
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btnPLUS
+        ' 
+        btnPLUS.Location = New Point(298, 372)
+        btnPLUS.Name = "btnPLUS"
+        btnPLUS.Size = New Size(60, 60)
+        btnPLUS.TabIndex = 9
+        btnPLUS.Text = "+"
+        btnPLUS.UseVisualStyleBackColor = True
+        ' 
+        ' btnMOINS
+        ' 
+        btnMOINS.Location = New Point(298, 296)
+        btnMOINS.Name = "btnMOINS"
+        btnMOINS.Size = New Size(60, 60)
+        btnMOINS.TabIndex = 10
+        btnMOINS.Text = "-"
+        btnMOINS.UseVisualStyleBackColor = True
+        ' 
+        ' btnFOIS
+        ' 
+        btnFOIS.Location = New Point(298, 220)
+        btnFOIS.Name = "btnFOIS"
+        btnFOIS.Size = New Size(60, 60)
+        btnFOIS.TabIndex = 11
+        btnFOIS.Text = "*"
+        btnFOIS.UseVisualStyleBackColor = True
+        ' 
+        ' btnAC
+        ' 
+        btnAC.Location = New Point(113, 134)
+        btnAC.Name = "btnAC"
+        btnAC.Size = New Size(70, 57)
+        btnAC.TabIndex = 12
+        btnAC.Text = "AC"
+        btnAC.UseVisualStyleBackColor = True
+        ' 
+        ' btnRT
+        ' 
+        btnRT.Location = New Point(199, 134)
+        btnRT.Name = "btnRT"
+        btnRT.Size = New Size(70, 57)
+        btnRT.TabIndex = 13
+        btnRT.Text = "="
+        btnRT.UseVisualStyleBackColor = True
+        ' 
+        ' btnSUR
+        ' 
+        btnSUR.Location = New Point(298, 134)
+        btnSUR.Name = "btnSUR"
+        btnSUR.Size = New Size(60, 60)
+        btnSUR.TabIndex = 14
+        btnSUR.Text = "/"
+        btnSUR.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Location = New Point(25, 134)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(70, 70)
+        btn0.TabIndex = 15
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' btnCLOSE
+        ' 
+        btnCLOSE.Location = New Point(25, 24)
+        btnCLOSE.Name = "btnCLOSE"
+        btnCLOSE.Size = New Size(70, 70)
+        btnCLOSE.TabIndex = 16
+        btnCLOSE.Text = "Close"
+        btnCLOSE.UseVisualStyleBackColor = True
+        ' 
+        ' txt
+        ' 
+        txt.Location = New Point(158, 24)
+        txt.Multiline = True
+        txt.Name = "txt"
+        txt.Size = New Size(200, 70)
+        txt.TabIndex = 17
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(378, 452)
+        Controls.Add(txt)
+        Controls.Add(btnCLOSE)
+        Controls.Add(btn0)
+        Controls.Add(btnSUR)
+        Controls.Add(btnRT)
+        Controls.Add(btnAC)
+        Controls.Add(btnFOIS)
+        Controls.Add(btnMOINS)
+        Controls.Add(btnPLUS)
+        Controls.Add(btn9)
+        Controls.Add(btn8)
+        Controls.Add(btn7)
+        Controls.Add(btn6)
+        Controls.Add(btn5)
+        Controls.Add(btn4)
+        Controls.Add(btn3)
+        Controls.Add(btn2)
+        Controls.Add(btn1)
+        Name = "Form1"
+        Text = "calculatrice"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnPLUS As Button
+    Friend WithEvents btnMOINS As Button
+    Friend WithEvents btnFOIS As Button
+    Friend WithEvents btnAC As Button
+    Friend WithEvents btnRT As Button
+    Friend WithEvents btnSUR As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnCLOSE As Button
+    Friend WithEvents txt As TextBox
+
+End Class
---
+++ Calculatrice/Form1.vb
@@ -0,0 +1,94 @@
+﻿Public Class Form1
+    Dim a As Double
+    Dim b As Double
+    Dim op As String
+
+    Private Sub btnRT_Click(sender As Object, e As EventArgs) Handles btnRT.Click
+        b = Val(txt.Text)
+        If op = "+" Then
+            txt.Text = (a + b).ToString
+        End If
+        If op = "*" Then
+            txt.Text = (a * b).ToString
+        End If
+        If op = "-" Then
+            txt.Text = (a - b).ToString
+        End If
+        If op = "/" Then
+            txt.Text = (a / b).ToString
+        End If
+    End Sub
+
+    Private Sub btnPLUS_Click(sender As Object, e As EventArgs) Handles btnPLUS.Click
+        a = Val(txt.Text)
+        txt.Clear()
+        op = "+"
+    End Sub
+
+    Private Sub btnMOINS_Click(sender As Object, e As EventArgs) Handles btnMOINS.Click
+        a = Val(txt.Text)
+        txt.Clear()
+        op = "-"
+    End Sub
+
+    Private Sub btnFOIS_Click(sender As Object, e As EventArgs) Handles btnFOIS.Click
+        a = Val(txt.Text)
+        txt.Clear()
+        op = "*"
+    End Sub
+
+    Private Sub btnSUR_Click(sender As Object, e As EventArgs) Handles btnSUR.Click
+        a = Val(txt.Text)
+        txt.Clear()
+        op = "/"
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        txt.Text = txt.Text + "0"
+    End Sub
+
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        txt.Text = txt.Text + "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        txt.Text = txt.Text + "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        txt.Text = txt.Text + "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        txt.Text = txt.Text + "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        txt.Text = txt.Text + "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        txt.Text = txt.Text + "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        txt.Text = txt.Text + "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        txt.Text = txt.Text + "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        txt.Text = txt.Text + "9"
+    End Sub
+
+    Private Sub btnCLOSE_Click(sender As Object, e As EventArgs) Handles btnCLOSE.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnAC_Click(sender As Object, e As EventArgs) Handles btnAC.Click
+        txt.Clear()
+    End Sub
+End Class
